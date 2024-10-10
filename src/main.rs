8c16cf7e7716b0a0026f6d4566168276a147be7d
Anton <akumaigorodski@gmail.com>
2024-10-10 19:02:09 +0300

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,20 @@ fn main() {
     let data_user2 = -1.0;
     let data_user3 =  1.0;
 
+    // TX send
+
     // each user spreads their data before sending, more bandwidth is needed now
     // this is why CDMA is a essentially a frequency-spreading technique
     let signal_user1 = code_user1 * data_user1;
     let signal_user2 = code_user2 * data_user2;
     let signal_user3 = code_user3 * data_user3;
 
+    // RX receive
+
     // received signal is a superposition (vector sum) of all individual user signals
     let received_signal = DVector::<f64>::zeros(4) + signal_user1 + signal_user2 + signal_user3;
+
+    // RX decode, decide
 
     // leveraging orthogonality
     // spreading code eliminates everthing but related user data
